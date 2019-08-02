89fb0bf9830ff14a8bbdb4749d81cfac1c8b57af
a632079 <a632079@qq.com>
2019-08-02 23:47:02 +0800

    使用 lowDB 重构合并方法

--- core.ts
+++ core.ts
@@ -3,7 +3,7 @@ import nconf from 'nconf'
 import winston from 'winston'
 import fs from 'fs'
 // import path from 'path'
-import _ from 'lodash'
+// import _ from 'lodash'
 
 // 使用蓝鸟加速
 // import bluebird from 'bluebird'
@@ -21,7 +21,7 @@ import crypto from './src/crypto'
 // CronJob
 // import { CronJob } from 'cron'
 
-import { applyMinxin, StatusBody, NetworkError, DownServerListInterface, ServerListMember } from './src/utils'
+import { applyMerge, StatusBody, NetworkError, ServerListMember } from './src/utils'
 
 import Koa from 'koa'
 import Router from 'koa-router'
@@ -84,8 +84,8 @@ async function fetch (list: ServerListMember[]): Promise<(StatusBody | NetworkEr
         isError: true,
         id: input.id,
         code: -1, // 非网络错误
-        msg: err.message.replace(/(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]):[a-zA-Z0-9]+\d/g, 'Hidden IPAddress'),
-        stack: (err.stack || '').replace(/(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5]):[a-zA-Z0-9]+\d/g, 'Hidden IPAddress'),
+        msg: err.message.replace(ipv4Reg, 'Hidden IPAddress'),
+        stack: (err.stack || '').replace(ipv4Reg, 'Hidden IPAddress'),
         ts: Date.now()
       }
       return errorMsg
@@ -108,16 +108,12 @@ const childList: ChildList = {
   lastUpdate: 0,
   list: []
 }
-const downServerList: DownServerListInterface = fs.existsSync('./data/down.json') ? JSON.parse(fs.readFileSync('./data/down.json').toString()) : {
-  ids: [],
-  data: []
-}
 
 async function saveStatus (): Promise<void | Error> {
   if (!childList.lastUpdate) {
     childList.list = await fetchServerList()
     childList.lastUpdate = Date.now()
-  } else if ((Date.now() - childList.lastUpdate) > 60 * 60 * 2) {
+  } else if ((Date.now() - childList.lastUpdate) > 60 * 60 * 2 * 1000) {
     fetchServerList()
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       .then(list => {
@@ -138,58 +134,11 @@ async function saveStatus (): Promise<void | Error> {
       children.push((child as StatusBody))
     }
   }
-  // 迭代添加宕机时间
-  const toSaveIds = []
-  if (downServer.length > 0) {
-    for (const child of downServer) {
-      toSaveIds.push(child.id)
-      // 检测是否在目前的数据已经存在于宕机数组
-      if (_.indexOf(downServerList.ids, child.id) !== -1) {
-        // 已经存在于宕机数组
-        // 更新一下里面的部分信息
-        for (const solo of downServerList.data) {
-          if (solo.id === child.id) {
-            solo.statusMsg = child
-          }
-        }
-      } else {
-        // 并不存在， 我们添加进去
-        downServerList.ids.push(child.id)
-        downServerList.data.push({
-          id: child.id,
-          start: Date.now(),
-          statusMsg: child
-        })
-      }
-    }
-  }
-  // 移除已经失效的宕机数据
-  // console.log(downServerList.ids)
-  if (downServerList.data.length > 0) {
-    // console.log(downServerList.ids)
-    const toRemoveIds = _.difference(downServerList.ids, toSaveIds)
-    for (const index in downServerList.data) {
-      for (const id of toRemoveIds) {
-        if (id === downServerList.data[index].id) {
-          delete downServerList.data[index]
-        }
-      }
-    }
-    const bufferData = []
-    for (const data of downServerList.data) {
-      if (data !== undefined) {
-        bufferData.push(data)
-      }
-    }
-    _.pullAll(downServerList.ids, toRemoveIds)
-    downServerList.data = bufferData
-  }
-
   winston.verbose('执行数据合并...')
   // console.log(children)
   // console.log(downServer)
   // console.log(downServerList)
-  await applyMinxin(children, downServerList)
+  await applyMerge(children, downServer)
   // fs.existsSync(path.join('./data')) || fs.mkdirSync(path.join('./data'))
   // winston.info('写入状态数据...')
   // fs.writeFileSync(path.join('./data/status.json'), JSON.stringify(data))
@@ -206,8 +155,8 @@ function autoRestartSave (): void {
     .catch((err: any) => {
       const t = failtureRequestTimes + 1
       const i = defaultFailtrueInterval * (t ^ 2)
-      winston.warn('在合并状态过程中发生错误， 错误信息如下所示：')
-      winston.error(err)
+      winston.error('在合并状态过程中发生错误， 错误信息如下所示：')
+      console.error(err)
       winston.info(`自动重新尝试获取... 目前已失败 ${t} 次， 将在 ${i} 秒后重新尝试。`)
       failtureRequestTimes = t
       setTimeout(autoRestartSave, i * 1000);
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,79 @@
 import cmp from 'semver-compare'
+import lowdb from 'lowdb'
+import path from 'path'
+import FileSync from 'lowdb/adapters/FileSync'
+import _ from 'lodash'
 // import winston from 'winston'
 
+const statusAdapter = new FileSync(path.join('./', '../data/status.json'))
+const downServerListAdapter = new FileSync(path.join('./', '../data/down.json'))
+const db = {
+  status: lowdb(statusAdapter),
+  down: lowdb(downServerListAdapter)
+}
+
+// 指定数据库的默认值
+db
+  .status
+  .defaults({
+    version: '0.0.0',
+    children: [],
+    downServer: [],
+    status: {
+      load: [0, 0, 0],
+      memory: 0,
+      hitokoto: {
+        total: 0,
+        categroy: []
+      },
+      childStatus: []
+    },
+    requests: {
+      all: {
+        total: 0,
+        pastMinute: 0,
+        pastHour: 0,
+        pastDay: 0,
+        dayMap: [],
+        FiveMinuteMap: []
+      },
+      hosts: {
+        'v1.hitokoto.cn': {
+          total: 0,
+          pastMinute: 0,
+          pastHour: 0,
+          pastDay: 0,
+          dayMap: []
+        },
+        'api.hitokoto.cn': {
+          total: 0,
+          pastMinute: 0,
+          pastHour: 0,
+          pastDay: 0,
+          dayMap: []
+        },
+        'sslapi.hitokoto.cn': {
+          total: 0,
+          pastMinute: 0,
+          pastHour: 0,
+          pastDay: 0,
+          dayMap: []
+        }
+      }
+    },
+    lastUpdate: 0,
+    now: '',
+    ts: 0
+  })
+  .write()
+
+db
+  .down
+  .defaults({
+    ids: [],
+    lists: []
+  })
+  .write()
 export interface ServerListMember {
   url: string;
   id: string;
@@ -121,229 +194,163 @@ export interface DownServer {
   statusMsg: NetworkError;
 }
 
-export async function applyMinxin (
-  children: StatusBody[],
-  downServerList: DownServerListInterface
-): Promise<ExportData> {
-  // 首先初始化返回类
-  const result: ExportData = {
-    version: '0.0.0',
-    children: [],
-    downServer: [],
-    status: {
-      load: [0, 0, 0],
-      memory: 0,
-      hitokoto: {
-        total: 0,
-        categroy: []
-      },
-      childStatus: []
-    },
-    requests: {
-      all: {
-        total: 0,
-        pastMinute: 0,
-        pastHour: 0,
-        pastDay: 0,
-        dayMap: [],
-        FiveMinuteMap: []
-      },
-      hosts: {
-        'v1.hitokoto.cn': {
-          total: 0,
-          pastMinute: 0,
-          pastHour: 0,
-          pastDay: 0,
-          dayMap: []
-        },
-        'api.hitokoto.cn': {
-          total: 0,
-          pastMinute: 0,
-          pastHour: 0,
-          pastDay: 0,
-          dayMap: []
-        },
-        'sslapi.hitokoto.cn': {
-          total: 0,
-          pastMinute: 0,
-          pastHour: 0,
-          pastDay: 0,
-          dayMap: []
-        }
-      }
-    },
-    lastUpdate: 0,
-    now: '',
-    ts: 0
+
+function handleDownServerList (list: NetworkError[]): DownServerListInterface {
+  for (const child of list) {
+    if (db.down.get('ids').indexOf(child.id).value() < 0) { // 数据库里不存在
+      db.down.get('ids').push(child.id).value()
+      db.down.get('data')
+        .push({
+          id: child.id,
+          start: Date.now(),
+          statusMsg: child
+        })
+        .value()
+    } else { // 我们修改下信息
+      db.down.get('data')
+        .find({ id: child.id })
+        .assign({ statusMsg: child })
+    }
   }
+  // 反向检测是否正常
+  for (const id of db.down.get('ids').value()) {
+    if (!_.find(list, { id: id })) { // 机器恢复了
+      db.down.get('ids').pull(id).value()
+      db.down.get('data').remove({ id: id }).value()
+    }
+  }
+
+  db.down.write() // 写入文件
+  const r: DownServerListInterface = {
+    ids: db.down.get('ids').value(),
+    data: db.down.get('data').value()
+  }
+  return r
+}
+
+export async function applyMerge (
+  children: StatusBody[],
+  downServerList: NetworkError[]
+): Promise<void> {
+  // 初始化一些数据
+  let memory = 0
+  let hitokotoTotal = 0
+  let hitokotoCategroy: string[] = []
+  db.status.set('status.childStatus', []).value()
 
   // 注册一波缓存， 最蠢的数据合并方法
   const loadBuffer = [0, 0, 0]
-  let allDayMapBuffer: number[] = []
-  let allFiveMinuteBuffer: number[] = []
-  let v1DayMapBuffer: number[] = []
-  let apiDayMapBuffer: number[] = []
-  let sslapiDayMapBuffer: number[] = []
 
-  // 迭代数据集， 合并数据
+  // 迭代数据集
   for (const child of children) {
-    // 汇总子节点名称
-    result.children.push(child.server_id)
+    // 汇总服务器标识
+    const hasId = db.status.get('children').indexOf(child.server_id).value() // TODO: 追踪 Types: LowDB 的解决进度
+    if (!hasId) {
+      db.status.get('children').push(child.server_id).value()
+    }
 
     // 版本号
-    result.version =
-      cmp(result.version, child.version) < 0 ? child.version : result.version
+    const semVer = db.status.get('version').value()
+    if (cmp(semVer, child.version) < 0) {
+      db.status.set('version', child.version).value()
+    }
 
     // 缓存 status 统计, 以便结束迭代时计算平均值
     loadBuffer[0] += child.server_status.load[0]
     loadBuffer[1] += child.server_status.load[1]
     loadBuffer[2] += child.server_status.load[2]
 
-    // 汇总总使用内存
-    result.status.memory += child.server_status.memory.usage
+    // 汇总总占用内存
+    memory += child.server_status.memory.usage
 
     // 一言总数统计汇总
-    result.status.hitokoto.total =
-      result.status.hitokoto.total < child.server_status.hitokto.total
-        ? child.server_status.hitokto.total
-        : result.status.hitokoto.total
-    result.status.hitokoto.categroy =
-      result.status.hitokoto.categroy.length <
-      child.server_status.hitokto.categroy.length
-        ? child.server_status.hitokto.categroy
-        : result.status.hitokoto.categroy
+    if (hitokotoTotal < child.server_status.hitokto.total) hitokotoTotal = child.server_status.hitokto.total
 
-    // 推送子状态
-    const childStatus = {
-      id: child.server_id
-    }
-    result.status.childStatus.push(
-      Object.assign(childStatus, child.server_status)
-    )
+    // 一言分类汇总
+    if (hitokotoCategroy.length < child.server_status.hitokto.categroy.length) hitokotoCategroy = child.server_status.hitokto.categroy
 
-    // 合并 all 统计
-    result.requests.all.total += child.requests.all.total // 合并总请求数
-    result.requests.all.pastMinute += child.requests.all.pastMinute // 合并每分钟请求数
-    result.requests.all.pastHour += child.requests.all.pastHour // 合并每小时请求数
-    result.requests.all.pastDay += child.requests.all.pastDay // 合并每日请求数
-    if (allDayMapBuffer.length === 0) {
-      // 汇总 DayMap 统计
-      // 缓存为空
-      allDayMapBuffer = child.requests.all.dayMap
+    // 推送 childStatus
+    db.status.get('status.childStatus').push(child.server_status).value()
+
+    // 合并请求总数
+    db.status.get('status.requests.all')
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      .mapValues((v, k) => {
+        return v + child.requests.all[k] // Todo: 修复错误
+      })
+      .value()
+    if (db.status.get('requests.all.dayMap').size().value() === 0) { // 当日每小时请求数
+      db.status.set('requests.all.dayMap', child.requests.all.dayMap).value()
     } else {
-      // 缓存存在值
-      for (const index in child.requests.all.dayMap) {
-        allDayMapBuffer[index] += child.requests.all.dayMap[index]
-      }
+      db.status.get('requests.all.dayMap')
+        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+        .map((v, i) => {
+          return v + child.requests.all.dayMap[i]
+        })
+        .value()
     }
-    if (allFiveMinuteBuffer.length === 0) {
-      // 汇总 FiveMinuteMap 统计
-      // 缓存为空
-      allFiveMinuteBuffer = child.requests.all.FiveMinuteMap
+    if (db.status.get('requests.all.FiveMinuteMap').size().value() === 0) { // 过去 5 分钟每分钟请求数
+      db.status.set('requests.all.FiveMinuteMap', child.requests.all.dayMap).value()
     } else {
-      // 缓存存在值
-      for (const index in child.requests.all.FiveMinuteMap) {
-        allFiveMinuteBuffer[index] += child.requests.all.FiveMinuteMap[index]
-      }
+      db.status.get('requests.all.FiveMinuteMap')
+        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+        .map((v, i) => {
+          return v + child.requests.all.dayMap[i]
+        })
+        .value()
     }
 
-    // 汇总 hosts 统计
-    result.requests.hosts['v1.hitokoto.cn'].total +=
-      child.requests.hosts['v1.hitokoto.cn'].total
-    result.requests.hosts['v1.hitokoto.cn'].pastMinute +=
-      child.requests.hosts['v1.hitokoto.cn'].pastMinute
-    result.requests.hosts['v1.hitokoto.cn'].pastHour +=
-      child.requests.hosts['v1.hitokoto.cn'].pastHour
-    result.requests.hosts['v1.hitokoto.cn'].pastDay +=
-      child.requests.hosts['v1.hitokoto.cn'].pastDay
-    if (v1DayMapBuffer.length === 0) {
-      // 汇总 FiveMinuteMap 统计
-      // 缓存为空
-      v1DayMapBuffer = child.requests.hosts['v1.hitokoto.cn'].dayMap
-    } else {
-      // 汇总
-      for (const index in child.requests.hosts['v1.hitokoto.cn'].dayMap) {
-        v1DayMapBuffer[index] +=
-          child.requests.hosts['v1.hitokoto.cn'].dayMap[index]
-      }
-    }
-
-    if (typeof child.requests.hosts['api.hitokoto.cn'] !== 'undefined') {
-      result.requests.hosts['api.hitokoto.cn'].total +=
-        child.requests.hosts['api.hitokoto.cn'].total
-      result.requests.hosts['api.hitokoto.cn'].pastMinute +=
-        child.requests.hosts['api.hitokoto.cn'].pastMinute
-      result.requests.hosts['api.hitokoto.cn'].pastHour +=
-        child.requests.hosts['api.hitokoto.cn'].pastHour
-      result.requests.hosts['api.hitokoto.cn'].pastDay +=
-        child.requests.hosts['api.hitokoto.cn'].pastDay
-      if (apiDayMapBuffer.length === 0) {
-        // 汇总 FiveMinuteMap 统计
-        // 缓存为空
-        apiDayMapBuffer = child.requests.hosts['api.hitokoto.cn'].dayMap
-      } else {
-        // 汇总
-        for (const index in child.requests.hosts['api.hitokoto.cn'].dayMap) {
-          apiDayMapBuffer[index] +=
-            child.requests.hosts['api.hitokoto.cn'].dayMap[index]
-        }
-      }
-    }
-
-    if (typeof child.requests.hosts['sslapi.hitokoto.cn'] !== 'undefined') {
-      result.requests.hosts['sslapi.hitokoto.cn'].total +=
-        child.requests.hosts['sslapi.hitokoto.cn'].total
-      result.requests.hosts['sslapi.hitokoto.cn'].pastMinute +=
-        child.requests.hosts['sslapi.hitokoto.cn'].pastMinute
-      result.requests.hosts['sslapi.hitokoto.cn'].pastHour +=
-        child.requests.hosts['sslapi.hitokoto.cn'].pastHour
-      result.requests.hosts['sslapi.hitokoto.cn'].pastDay +=
-        child.requests.hosts['sslapi.hitokoto.cn'].pastDay
-      if (sslapiDayMapBuffer.length === 0) {
-        // 汇总 FiveMinuteMap 统计
-        // 缓存为空
-        sslapiDayMapBuffer = child.requests.hosts['sslapi.hitokoto.cn'].dayMap
-      } else {
-        // 汇总
-        for (const index in child.requests.hosts['sslapi.hitokoto.cn'].dayMap) {
-          sslapiDayMapBuffer[index] +=
-            child.requests.hosts['sslapi.hitokoto.cn'].dayMap[index]
-        }
-      }
-    }
+    // 合并 hosts 统计
+    db.status.get('status.requests.hosts')
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      .mapValues((hostData, host) => {
+        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+        _.mapValues(hostData, (value, key) => { // Todo: 修复错误
+          if (key === 'dayMap') {
+            if (value.length === 0) {
+              return child.requests.hosts[host][key]
+            } else {
+              // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+              return value.map((v, i) => {
+                return v + child.requests.hosts[host][key][i]
+              })
+            }
+          } else {
+            return value + child.requests.hosts[host][key]
+          }
+        })
+      })
+      .value()
   }
 
   // 计算 load 平均值
-  for (const index in loadBuffer) {
-    result.status.load[index] = loadBuffer[index] / result.children.length
-  }
+  db.status.get('status.load')
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    .map((v,i) => {
+      return loadBuffer[i] / result.children.length
+    })
 
-  // 合并 Map
-  result.requests.all.dayMap = allDayMapBuffer
-  result.requests.all.FiveMinuteMap = allFiveMinuteBuffer
-  result.requests.hosts['v1.hitokoto.cn'].dayMap = v1DayMapBuffer
-  result.requests.hosts['api.hitokoto.cn'].dayMap = apiDayMapBuffer
-  result.requests.hosts['sslapi.hitokoto.cn'].dayMap = sslapiDayMapBuffer
-
-  // 合并 宕机服务
-  if (downServerList.data.length > 0) {
-    for (const child of downServerList.data) {
-      result.children.push(child.id)
-      result.downServer.push({
+  // 合并宕机的服务器
+  const downList = handleDownServerList(downServerList)
+  if (downList.data.length > 0) {
+    for (const child of downList.data) {
+      db.status.get('children').push(child.id).value()
+      db.status.get('downServer').push({
         id: child.id,
         startTs: child.start,
         last: Date.now() - child.start,
         statusMessage: child.statusMsg
-      })
+      }).value()
     }
   }
 
   // 写入值
   const ts = Date.now()
   const date = new Date(ts)
-  result.lastUpdate = ts
-  result.now = date.toLocaleString()
-  result.ts = ts
+  db.status.set('lastUpdate', ts)
+  db.status.set('now', date.toLocaleString())
+  db.status.set('ts', ts)
 
-  return result
+  // 写入数据库
+  db.status.write()
 }
